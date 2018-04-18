a6424c8b2852e95706f3ac1f88672a8ca9c3089f
Tade <wangletian0131@hotmail.com>
2018-04-19 00:13:50 +0100

    lab7 finish, this is about Named entity recognition with Structured perceptron(with viterbi)

--- Lab7/lab7.py
+++ Lab7/lab7.py
@@ -75,16 +75,19 @@ class openfile:
                 #Word_Label_List.extend(tempList)
                 Word_Label_List.append(tempList)
         return Word_Label_List
-
+#random seed fixed, but I think there has a lot problem in decide the seed
+#learn it from lab4_solution
 random.seed(6)
 RRule = random.random()
 
+#create a new class to 
 class Word_Label:
     def __init__(self):
         self.word = None
         self.label = None
         self.previous = None
-        self.next = None 
+        self.next = None
+        self.sub = None 
 
 class Binary_perceptron:
     def __init__(self, train, test, multipalTime):
@@ -101,8 +104,9 @@ class Binary_perceptron:
             random.shuffle(trainData, lambda: RRule)
             for item in trainData:
                 for node in item:
-                    if not self.predict(node) == node.label:
-                        self.update(node)
+                    predictLabel = self.predict(node)
+                    if not predictLabel == node.label:
+                        self.update(node, predictLabel)
         
     def predict(self, node):
         if node.word in self.weight.keys():
@@ -110,12 +114,14 @@ class Binary_perceptron:
         else:
             return 'O'
 
-    def update(self, node):
+    def update(self, node, predictLabel):
         labelList = {'O': 0, 'PER': 0, 'LOC': 0, 'ORG': 0, 'MISC': 0}
         if node.word in self.weight.keys():
             self.weight[node.word][node.label] += 1/self.multipalTime
+            self.weight[node.word][predictLabel] -= 1/self.multipalTime
         else:
             labelList[node.label] += 1/self.multipalTime
+            labelList[predictLabel] -= 1/self.multipalTime
             self.weight[node.word] = labelList
 
     def testProcess(self):
@@ -139,8 +145,8 @@ class Binary_perceptron:
         print('The List of ORG top 10 is :\n',[item for item in sorted(self.weight, key = lambda i : self.weight[i]['ORG'],reverse=True)[:10]])
         print('The List of MISC top 10 is :\n',[item for item in sorted(self.weight, key = lambda i : self.weight[i]['MISC'],reverse=True)[:10]])
 
-
-class structuredPerceptron:
+        
+class structuredPerceptronLabel:
     def __init__(self, train, test, multipalTime, subFeatures):
         self.multipalTime = multipalTime
         self.train = train
@@ -165,6 +171,8 @@ class structuredPerceptron:
                 if not predictstructuredLabel == [node.label for node in item]:
                     self.update(item, predictstructuredLabel)
 
+#I divide predict process into two part, one part for the first two process,
+#after that, I repeat a viterbi process
     def predict(self, nodeList):
         firstColumn = {}
         secondColumn = {}
@@ -174,7 +182,13 @@ class structuredPerceptron:
         else:
             firstColumn = {'O': 0, 'PER': 0, 'LOC': 0, 'ORG': 0, 'MISC': 0}
         if len(nodeList) == 1 :
-            return [sorted(firstColumn, key = lambda i : firstColumn[i],reverse = True)[0]]
+            if self.subFeatures['number']:
+                if self.numberJudge(nodeList[0]):
+                    return ['O']
+                else:
+                    return [sorted(firstColumn, key = lambda i : firstColumn[i],reverse = True)[0]]
+            else:
+                return [sorted(firstColumn, key = lambda i : firstColumn[i],reverse = True)[0]]
         if nodeList[1].word in self.weight['wordLabel'].keys():
             for item in firstColumn.keys():
                 for label in self.weight['wordLabel'][nodeList[1].word].keys():
@@ -183,9 +197,19 @@ class structuredPerceptron:
         else:
             predictLabel = [sorted(firstColumn, key = lambda i : firstColumn[i],reverse = True)[0],'O']
         if len(nodeList) == 2:
-            return predictLabel
+            if self.subFeatures['number']:
+                for q in range(len(nodeList)):
+                    if self.numberJudge(nodeList[q]):
+                        predictLabel[q] = 'O'
+                return predictLabel
+            else:
+                return predictLabel
         for i in range(len(nodeList)-2):
             predictLabel.extend(self.viterbi(i, predictLabel[-1], nodeList))
+        if self.subFeatures['number']:
+            for m in range(len(nodeList)):
+                if self.numberJudge(nodeList[m]):
+                    predictLabel[m] = 'O'
         return predictLabel
 
     def viterbi(self,time , previousLabel, nodeList):
@@ -216,7 +240,7 @@ class structuredPerceptron:
             self.weight['previousLabel'][nodeLabelList[i]][nodeLabelList[i+1]] += 1/self.multipalTime
             self.weight['previousLabel'][predictLabelList[i]][predictLabelList[i+1]] -= 1/self.multipalTime
 
-        
+    #weight stucture 
         #weight = weight = {
                         #   'wordLabel' :{'word':{'O':1,'PER':2,...,'MISC':2},}, 
                         #   'previousLabel':{'start':{'O':1,'PER':2,...,'MISC':2},
@@ -239,7 +263,10 @@ class structuredPerceptron:
             
         f1_micro = f1_score(correct, predicted, average='micro', labels=['ORG', 'MISC', 'PER', 'LOC'])
         print('----------------------------------------')
-        print('current word-current label & previous label-current label\nfi_micro is:',f1_micro)
+        if self.subFeatures['number']:
+            print('current word-current label & previous label-current label\nwith number pre-process\nfi_micro is:',f1_micro)
+        else:
+            print('current word-current label & previous label-current label\nfi_micro is:',f1_micro)
         print('----------------------------------------')
         print('The List of O top 10 is : \n',[item for item in sorted(Counter(top10['O']), key = lambda i : Counter(top10['O'])[i], reverse = True)[:10]])
         print('The List of PER top 10 is : \n',[item for item in sorted(Counter(top10['PER']), key = lambda i : Counter(top10['PER'])[i], reverse = True)[:10]])
@@ -248,14 +275,99 @@ class structuredPerceptron:
         print('The List of MISC top 10 is : \n',[item for item in sorted(Counter(top10['MISC']), key = lambda i : Counter(top10['MISC'])[i], reverse = True)[:10]])
         print('----------------------------------------')
 
-
     def numberJudge(self, node):
         for item in node.word:
             if item.isdigit():
                 return True
         return False
 
+#this project I choose sub word feature
+class sub_perceptron:
+    def __init__(self, train, test, multipalTime):
+        self.multipalTime = multipalTime
+        self.train = train
+        self.test = test
+        self.weight = {}
+        #weight = {'word':{'ed':{'O':1,'PER':2,...,'MISC':2}
+                          #{'ing':{'O':1,'PER':2,...,'MISC':2}},
+                          #                          }
+        self.trainProcess()
+        self.testProcess()
+        
+    def trainProcess(self):
+        for i in range(self.multipalTime):
+            trainData = self.train
+            random.shuffle(trainData, lambda: RRule)
+            for item in trainData:
+                for node in item:
+                    self.sub(node)
+                    predictLabel = self.predict(node)
+                    if not predictLabel == node.label:
+                        self.update(node, predictLabel)
+        
+    def predict(self, node):
+        if node.word in self.weight.keys():
+            if node.sub in self.weight[node.word]:
+                return sorted(self.weight[node.word][node.sub],key = lambda i : self.weight[node.word][node.sub][i], reverse = True)[0]
+            else:
+                return 'O'
+        else:
+            return 'O'
 
+    def update(self, node, predictLabel):
+        labelList = {'O': 0, 'PER': 0, 'LOC': 0, 'ORG': 0, 'MISC': 0}
+        if node.word in self.weight.keys():
+            if node.sub in self.weight[node.word].keys():
+                self.weight[node.word][node.sub][node.label] += 1/self.multipalTime
+                self.weight[node.word][node.sub][predictLabel] -= 1/self.multipalTime
+            else:
+                labelList[node.label] += 1/self.multipalTime
+                labelList[predictLabel] -= 1/self.multipalTime
+                self.weight[node.word][node.sub] = labelList
+        else:
+            labelList[node.label] += 1/self.multipalTime
+            labelList[predictLabel] -= 1/self.multipalTime
+            self.weight[node.word] = {}
+            self.weight[node.word][node.sub] = labelList
+
+    def testProcess(self):
+        correct = []
+        predicted = []
+        for item in self.test:
+            for node in item:
+                self.sub(node)
+                correct.append(node.label)
+                predicted.append(self.predict(node))
+        f1_micro = f1_score(correct, predicted, average='micro', labels=['ORG', 'MISC', 'PER', 'LOC'])
+        print('----------------------------------------')
+        print('sub word feature \nfi_micro is:',f1_micro)
+        print('----------------------------------------')
+        Odict = self.findDict('O')
+        print('The List of O top 10 is :\n',[item for item in sorted(Odict, key = lambda i : Odict[i],reverse=True)[:10]])
+        PERdict = self.findDict('PER')
+        print('The List of PER top 10 is :\n',[item for item in sorted(PERdict, key = lambda i : PERdict[i],reverse=True)[:10]])
+        LOCdict = self.findDict('LOC')
+        print('The List of LOC top 10 is :\n',[item for item in sorted(LOCdict, key = lambda i : LOCdict[i],reverse=True)[:10]])
+        ORGdict = self.findDict('ORG')
+        print('The List of ORG top 10 is :\n',[item for item in sorted(ORGdict, key = lambda i : ORGdict[i],reverse=True)[:10]])
+        MISCdict = self.findDict('MISC')
+        print('The List of MISC top 10 is :\n',[item for item in sorted(MISCdict, key = lambda i : MISCdict[i],reverse=True)[:10]])
+        print('----------------------------------------')
+
+    def findDict(self, name):
+        dic = {}
+        for word in self.weight:
+            count = 0
+            for sub in self.weight[word]:
+                count += self.weight[word][sub][name]
+            dic[word] = count
+        return dic
+
+    def sub(self,node):
+        if len(node.word) >= 3 :
+            node.sub = node.word[-3:]
+        else:
+            node.sub = node.word
 
 if __name__ == '__main__': 
     start = timeit.default_timer()
@@ -263,8 +375,11 @@ if __name__ == '__main__':
     #print(config.args)
     openFile = openfile(config.args)
     multipalTime = 10
-    bigram = Binary_perceptron(openFile.train, openFile.test, multipalTime)
     subFeatures = {'number':False}
-    structuredPrevious = structuredPerceptron(openFile.train, openFile.test, multipalTime, subFeatures)
+    bigram = Binary_perceptron(openFile.train, openFile.test, multipalTime)
+    structuredPrevious = structuredPerceptronLabel(openFile.train, openFile.test, multipalTime, subFeatures)
+    subFeatures = {'number':True}
+    structuredPrevious = structuredPerceptronLabel(openFile.train, openFile.test, multipalTime, subFeatures)
+    sub_perceptron = sub_perceptron(openFile.train, openFile.test, multipalTime)
     elapsed = timeit.default_timer() - start
     print('The program take',elapsed,'seconds to complete.')
